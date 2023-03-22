d25a010133a5f5f1d1ef0b3809d3a0e56b1bb7d1
oroszlanolo <balazsi.balint1111@gmail.com>
2023-03-22 16:15:17 +0100

    Mark all as read functionality

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ import data from './data';
 
 function App() {
   const [posts, setPosts] = useState(data);
+  const markAsRead = () => {
+    setPosts(prev => prev.map(p => {return {...p, new: false}}));
+  }
   return (
     <div className='bg-neutral-300 w-screen h-screen pt-20'>
       <div className={`max-w-2xl m-auto
@@ -15,7 +18,7 @@ function App() {
           <p className=' font-bold mr-2 text-2xl'>Notifications</p>
           <p className=' text-neutral-white bg-primary-blue px-2.5 text-xs leading-none font-bold py-1 rounded-md'>3</p>
           <div className='grow'/>
-          <button className=' text-neutral-500 text-md'>Mark all as read</button>
+          <button className=' text-neutral-500 text-md' onClick={markAsRead}>Mark all as read</button>
         </div>
         <div className='flex flex-col gap-2'>
           {posts.map((post, idx) => <Bubble key={idx} post={post}/>)}
